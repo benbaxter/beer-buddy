6272710422c0e6f0c2f64d40387ddb09cbd85165
rotht2 <rotht2@nku.edu>
2015-04-27 18:52:02 -0400

    added the search and filter bar backend

--- beer-buddy-core/src/main/java/com/beerbuddy/core/repository/BeerRepository.java
+++ beer-buddy-core/src/main/java/com/beerbuddy/core/repository/BeerRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.beerbuddy.core.model.Beer;
 
@@ -14,5 +15,14 @@ public interface BeerRepository extends JpaRepository<Beer, Long> {
 	@Query("select distinct (b.type) from Beer b order by b.type asc")
 	public List<String> findTypes();
 	
-	public Page<Beer> findByType(String type, Pageable page);
+	//TODO: write the query and declare some params to use with it 
+	//@Query("select from Beer b where b.type = ?1 order by b.name asc")
+	public Page<Beer> findByType( String type, Pageable page);
+	
+	//TODO: write the query for search
+	
+	///@Query("select b from Beer where b.name like %:nameString%")
+	public Page<Beer> findByNameContaining(String name, Pageable page);
+	
+	public Page<Beer> findByNameContainingAndType(String name, String type, Pageable page);
 }
--- beer-buddy-core/src/test/java/com/beerbuddy/core/repository/BeerRepositoryIntegrationTest.java
+++ beer-buddy-core/src/test/java/com/beerbuddy/core/repository/BeerRepositoryIntegrationTest.java
@@ -29,63 +29,72 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import com.beerbuddy.core.model.Beer;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes={TestRepositoryConfig.class})
+@ContextConfiguration(classes = { TestRepositoryConfig.class })
 public class BeerRepositoryIntegrationTest {
 
 	@Autowired
 	BeerRepository repository;
-	
+
 	@Autowired
 	private DataSource dataSource;
-	
+
 	private IDatabaseTester databaseTester;
-	
+
 	@Before
 	public void setup() throws Exception {
 		databaseTester = new DataSourceDatabaseTester(dataSource);
-		
+
 		IDataSet dataSet = null;
 
 		dataSet = new FlatXmlDataFileLoader().load("/beer-repo-dataset.xml");
-		
-		
+
 		databaseTester.setDataSet(dataSet);
-		
+
 		databaseTester.onSetup();
 	}
-	
+
 	@After
 	public void tearDown() throws Exception {
 		databaseTester.onTearDown();
 	}
-	
+
+	@Test
+	public void findType() {
+		List<String> types = repository.findTypes();
+		// System.out.println(types.toString()); //without distinct, retrieved:
+		// [Malt, Malt, Malt, Malt, Malt, Ale]
+		// The XML document for the test beers had one ale and 5 malts. I
+		// modified the XML document to contain 3 extra types
+		assertThat(types.size(), is(equalTo(2)));
+	}
+
 	@Test
 	public void findByType() {
 		String type = "Malt";
 		Pageable pageRequest = new PageRequest(0, 1);
-		
+
 		Page<Beer> page = repository.findByType(type, pageRequest);
-		
+
 		assertThat(page.getNumber(), is(equalTo(0)));
 		assertThat(page.getNumberOfElements(), is(equalTo(1)));
 		assertThat(page.getSize(), is(equalTo(1)));
-		
+
 		List<Beer> content = page.getContent();
 		assertThat(content, is(not(nullValue())));
 		assertThat(content.size(), is(equalTo(1)));
 		Beer beer = content.get(0);
 		assertThat(beer, is(not(nullValue())));
 		assertThat(beer.getType(), is(equalTo(type)));
-		
-		//test all beers returned are of the correct type...
+
+		// test all beers returned are of the correct type...
 		pageRequest = new PageRequest(0, 100);
-		
+
 		page = repository.findByType(type, pageRequest);
-		
+
 		assertThat(page.getNumber(), is(equalTo(0)));
 		assertThat(page.getNumberOfElements(), is(equalTo(5)));
 		assertThat(page.getSize(), is(equalTo(100)));
-		
+
 		content = page.getContent();
 		assertThat(content, is(not(nullValue())));
 		assertThat(content.size(), is(equalTo(5)));
@@ -94,43 +103,87 @@ public class BeerRepositoryIntegrationTest {
 			assertThat(b.getType(), is(equalTo(type)));
 		});
 	}
+
+	@Test
+	public void findByNameContaining() {
+		String name = "Six";
+		Pageable pageRequest = new PageRequest(0, 100);
+		Page<Beer> page = repository.findByNameContaining(name, pageRequest);
+		assertThat(page.getNumberOfElements(), is(equalTo(1)));
+		List<Beer> content = page.getContent();
+		assertThat(content, is(not(nullValue())));
+		assertThat(content.size(), is(equalTo(1)));
+		Beer beer = content.get(0);
+		assertThat(beer, is(not(nullValue())));
+		assertThat(beer.getName(), is(equalTo("My Sixth Beer")));
+
+	}
 	
+	@Test
+	public void findByNameAndFilterAle(){
+		String name = "Beer";
+		String type = "Ale";
+		Pageable pageRequest = new PageRequest(0,100);
+		Page<Beer> page = repository.findByNameContainingAndType(name, type, pageRequest);
+		assertThat(page.getNumberOfElements(), is(equalTo(1)));
+		List<Beer> content = page.getContent();
+		assertThat(content, is(not(nullValue())));
+		assertThat(content.size(), is(equalTo(1)));
+		Beer beer = content.get(0);
+		assertThat(beer, is(not(nullValue())));
+		assertThat(beer.getName(), is(equalTo("My First Beer")));
+	}
 	
-	@Ignore
+	@Test
+	public void findByNameAndFilterMalt(){
+		String name = "Beer";
+		String type = "Malt";
+		Pageable pageRequest = new PageRequest(0,100);
+		Page<Beer> page = repository.findByNameContainingAndType(name, type, pageRequest);
+		assertThat(page.getNumberOfElements(), is(equalTo(5)));
+		List<Beer> content = page.getContent();
+		assertThat(content, is(not(nullValue())));
+		assertThat(content.size(), is(equalTo(5)));
+		Beer beer = content.get(0);
+		assertThat(beer, is(not(nullValue())));
+		assertThat(beer.getName(), is(equalTo("My Sixth Beer")));
+	}
+	
 	@Test
 	public void findByType_badTestThatNeedsToBeFixed() {
 		String type = "Ale";
 		Pageable pageRequest = new PageRequest(0, 1);
-		
+
 		Page<Beer> page = repository.findByType(type, pageRequest);
-		
+
 		assertThat(page.getNumber(), is(equalTo(0)));
 		assertThat(page.getNumberOfElements(), is(equalTo(1)));
 		assertThat(page.getSize(), is(equalTo(1)));
-		
+
 		List<Beer> content = page.getContent();
 		assertThat(content, is(not(nullValue())));
 		assertThat(content.size(), is(equalTo(1)));
 		Beer beer = content.get(0);
 		assertThat(beer, is(not(nullValue())));
 		assertThat(beer.getType(), is(equalTo(type)));
-		
-		//test all beers returned are of the correct type...
+
+		// test all beers returned are of the correct type...
 		pageRequest = new PageRequest(0, 100);
-		
+
 		page = repository.findByType(type, pageRequest);
-		
+
+		content = page.getContent();
+
 		assertThat(page.getNumber(), is(equalTo(0)));
-		assertThat(page.getNumberOfElements(), is(equalTo(5)));
+		assertThat(page.getNumberOfElements(), is(equalTo(1)));
 		assertThat(page.getSize(), is(equalTo(100)));
-		
 		content = page.getContent();
 		assertThat(content, is(not(nullValue())));
-		assertThat(content.size(), is(equalTo(5)));
+		assertThat(content.size(), is(equalTo(1)));
 		page.forEach(b -> {
 			assertThat(b, is(not(nullValue())));
 			assertThat(b.getType(), is(equalTo(type)));
 		});
 	}
-	
+
 }
